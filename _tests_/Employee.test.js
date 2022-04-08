da282992126576785b8209e5387b0636ef648849
JulianF <floydjulian1991@gmail.com>
2022-04-08 15:16:56 -0700

    still testin' employee

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -2,10 +2,12 @@
 const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
-    const employee = new Employee('Julian');
+    const employee = new Employee('Julian', 'floydjulian1991@gmail.com');
 
     expect(employee.name).toBe('Julian');
-    
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.role).toBe('Employee');
+    expect(employee.email).toBe('floydjulian1991@gmail.com');
 });
 
 
