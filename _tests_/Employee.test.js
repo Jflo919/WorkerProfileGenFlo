6939724c17e9825613950f1f8ab7c962fc9333ef
JulianF <floydjulian1991@gmail.com>
2022-04-08 14:14:44 -0700

    got employee to pass initial test on one test criteria, create employee object...

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,4 +1,12 @@
+
 const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Julian');
+
+    expect(employee.name).toBe('Julian');
+    
+});
 
 
 
