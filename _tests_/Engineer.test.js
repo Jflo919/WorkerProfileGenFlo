703d360e4f30e793a7a321558b8fee2dd6b32eb2
JulianF <floydjulian1991@gmail.com>
2022-04-10 12:50:27 -0700

    added two more passing tests to engineer

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -7,3 +7,15 @@ test ('create an Engineer object', () => {
     expect(engineer.githubs).toBe('Jflo919');
     expect(engineer.role).toBe('Engineer');
 });
+
+test("Gets Engineer's GitHub Username", () => {
+    const engineer = new Engineer('Julian', '', '' ,'Jflo919', 'Engineer');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining('Jflo919'));
+});
+
+test("Gets Engineer's Role", () => {
+    const engineer = new Engineer('Julian', '', '' ,'Jflo919', 'Engineer');
+
+    expect(engineer.getRole()).toEqual(expect.stringContaining('Engineer'));
+});
