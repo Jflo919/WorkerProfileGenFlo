267aea1e886a9f3831c92a70e1d09e5bbd0bc8ea
JulianF <floydjulian1991@gmail.com>
2022-04-10 13:14:59 -0700

    Manager passed test in jest

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,1 +1,15 @@
 const Manager = require('../lib/Manager.js');
+const Employee = require('../lib/Employee');
+
+test('Create a Manager object', () => {
+    const manager = new Manager('', '', '', 'Manager', 50);
+
+    expect(manager.role).toBe('Manager');
+    expect(manager.officeNumber).toBe(50);
+});
+
+test("Get Manager's role", () => {
+    const manager = new Manager('', '', '', 'Manager', 50);
+
+    expect(manager.getRole()).toEqual(expect.stringContaining('Manager'));
+});
