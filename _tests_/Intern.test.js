4b808abdb8ed890a86caf0bee4f509e125d04662
JulianF <floydjulian1991@gmail.com>
2022-04-10 12:59:16 -0700

    got intern to fail, then pass first test

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,1 +1,9 @@
 const Intern = require('../lib/Intern.js');
+const Employee = require('../lib/Employee');
+
+test('create an Intern object', () => {
+    const intern = new Intern('', '', '', 'Intern', 'UCSC');
+
+    expect(intern.role).toBe('Intern');
+    expect(intern.school).toBe('UCSC');
+});
