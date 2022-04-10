a1c10fdc909cd9ab313dd3d7b9c622ae9ff2a854
JulianF <floydjulian1991@gmail.com>
2022-04-10 12:36:04 -0700

    got engineer to pass test

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,1 +1,9 @@
-const Engineer = require('../lib/Engineer.js');
+const Engineer = require('../lib/Engineer');
+const Employee = require('../lib/Employee');
+
+test ('create an Engineer object', () => {
+    const engineer = new Engineer('Julian', '', '' ,'Jflo919', 'Engineer');
+    expect(engineer.name).toBe('Julian');
+    expect(engineer.githubs).toBe('Jflo919');
+    expect(engineer.role).toBe('Engineer');
+});
